17d64a820d10aa7817f408ae6fc9ae19ddcbb7ab
Luc de Wit <luc.dewit@hotmail.nl>
2022-10-08 00:23:35 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app-wrapper">
+    <div id="app-title">Listing of all my portefolio projects:</div>
+
+    <br /><br /><br />
+
+    <div id="card-container">
+      <Card
+        title="Articles"
+        image="https://media.discordapp.net/attachments/375936269907263489/1027949682510532708/unknown.png?width=401&height=401"
+        link="https://medium.com/@lukedev_"
+        description="I sometimes like to write about programming things ive learned and experienced in my career and learning journey. <br /><br /> Im still new to writing articles and really enjoy learning to write. <br /><br /> I post these articles to my medium account."
+      />
+
+      <Card
+        title="Luke dev website"
+        image="https://media.discordapp.net/attachments/375936269907263489/1028064521035726898/unknown.png?width=401&height=401"
+        link="https://lukedev.nl/"
+        description="My main website that acts as my portefolio but also hosts most of my projects. <br /><br /> On here you can also find a nice contact form I made for myself, which uses discord webhooks to notify me."
+      />
+
+      <Card
+        title="COD Language"
+        image="https://media.discordapp.net/attachments/375936269907263489/1028065908238864434/unknown.png"
+        link="https://github.com/justlucdewit/cod"
+        description="The COD programming language is a transpiled language that I spend some time on making after getting inspired by the porth series of tsoding on youtube. <br /><br /> The language is a concatinative stack based language which means it is a pretty excentric yet interesting langugae. <br /><br /> Right now the compiler is written in python but I am planning to bootstrap it."
+      />
+
+      <Card
+        title="Galaxy generator"
+        image="https://media.discordapp.net/attachments/375936269907263489/1028066094843445328/unknown.png?width=401&height=401"
+        link="https://justlucdewit.github.io/galaxy-map/"
+        description="This galaxy generator is something I made for a worldbuilding group together with my friend <a href='https://github.com/Mateiadrielrafael'>Matei Adriel</a>. <br /><br /> I started the project and designed the algorithm to randomly pick coordinates with a distribution in the shape of a spiral galaxy, and Matei helped me with the design of the UI."
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "@/components/ProjectCard.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#card-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+
+  @media screen and (max-width: 1400px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+#app-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  margin: 1rem;
+}
+
+#app-title {
+  font-size: x-large;
+  font-weight: bold;
 }
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-header-title">
+          {{ title }}
+        </h3>
+        
+        <img :src="image" alt="" class="card-img">
+      </div>
+      <div class="card-content">
+        <div class="content" v-html="description">
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a :href="link" class="card-footer-item" target="_blank">View</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        description: String,
+        link: String,
+        image: String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.card {
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 0.5rem;
+    overflow: hidden;
+    width: 300px;
+    height: 600px;
+    background-color: #f5f5f5;
+    position: relative;
+}
+
+.card-header {
+    background-color: #f5f5f5;
+    padding: 1rem;
+}
+
+.card-img {
+    width: 270px;
+    height: 200px;
+    object-fit: contain;
+    margin-top: 20px;
+}
+
+.card-header-title {
+    margin: 0;
+    font-size: 1.25rem;
+    font-weight: 700;
+}
+
+.card-content {
+    padding: 1rem;
+    background: #f5f5f5;
+    text-align: left;
+}
+
+.card-footer {
+    background-color: #f5f5f5;
+    padding: 1rem;
+    
+    /* put at bottom */
+    position: absolute;
+    bottom: 0;
+}
+
+.card-footer-item {
+    color: #4a4a4a;
+    text-decoration: none;
+    font-weight: 700;
+    padding: 0.5rem 1rem;
+    border-radius: 0.25rem;
+    transition: all 0.3s ease-out;
+}
+</style>
